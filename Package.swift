641c9809b2807fb26d7d5d297277c9e95550bfa4
Paulo Faria <paulo.faria.rl@gmail.com>
2016-06-03 00:04:58 -0300

    split from websocket

--- Package.swift
+++ Package.swift
@@ -1,13 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "WebSocket",
+    name: "WebSocketServer",
     dependencies: [
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/VeniceX/HTTPSClient.git", Version(0, 7, 1)),
-        .Package(url: "https://github.com/Zewo/Event.git",  Version(0, 5, 1)),
-        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/UUID.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 0),
     ]
 )
