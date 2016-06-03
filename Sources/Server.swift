b4bb9c607f87e2c712f8969ff3111f77f4eaa0f4
Paulo Faria <paulo.faria.rl@gmail.com>
2016-06-03 00:11:11 -0300

    export WebSocket

--- Sources/Server.swift
+++ Sources/Server.swift
@@ -22,7 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import WebSocket
+@_exported import WebSocket
 @_exported import HTTP
 
 public struct Server: Responder, Middleware {
