d233248dc38fdf4e68b4810f32d1467e2bb606a7
Alex Studnička <astudnicka@gmail.com>
2016-06-12 11:44:26 +0200

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "WebSocketServer",
     dependencies: [
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 8),
     ]
 )
